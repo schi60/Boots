70a20138dcbc3bb78f55ef69f95f61b7b7b9777f
emaan asif <emaanasif438@gmail.com>
2025-12-07 18:09:42 -0500

    db

--- app/build_db.py
+++ app/build_db.py
@@ -1,19 +1,21 @@
+# Sophia Chi, Emaan Asif, Jun Jie Li
+# SoftDev
+# P01
+# Dec 2025
+
 import sqlite3
 
-DB_FILE = "data.db"
+DB_FILE="data.db"
 
-def create_tbs():
-    db = sqlite3.connect(DB_FILE)
-    c = db.cursor()
-    tb_names = ["USER", "API_CACHE"]
-    for tb in tb_names:
-        c.execute(f"DROP TABLE IF EXISTS {tb}")
+db = sqlite3.connect(DB_FILE)
+c = db.cursor()
 
-    c.execute(f"CREATE TABLE USER(user_id TEXT PRIMARY KEY, password TEXT)")
-    c.execute(f"CREATE TABLE API_CACHE(id TEXT PRIMARY KEY, user_id TEXT, name TEXT, path TEXT)")
-    db.commit()
-    db.close()
+c.executescript("""
+DROP TABLE IF EXISTS profiles;
+CREATE TABLE profiles (
+    username TEXT PRIMARY KEY, 
+    password TEXT
+);""")
 
-if __name__ == "__main__":
-    create_tbs()
-    print("Database created successfully.")
+db.commit()
+db.close()
