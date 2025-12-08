4e562e7af9a9fa857752cd6faee1b4a987af60d6
emaanas1f <emaana3@nycstudents.net>
2025-12-08 10:57:42 -0500

    pushing api work and devlog

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@
 
 from flask import Flask, render_template, request, flash, redirect, session, url_for
 from db import select_query, insert_query, general_query
-import sqlite3
+import sqlite3, urllib, json
 
 app = Flask(__name__)
 app.secret_key = "edkasifjdsufh"
@@ -18,29 +18,54 @@ def check_authentification():
         flash("Please log in to view our website", "error")
         return redirect(url_for("auth.login_get"))
 
+#home
 @app.get('/')
 def home_get():
     return render_template('frontlawn.html')
 
+#bedroom
 @app.get('/bedroom')
 def bedroom_get():
     return render_template('bedroom.html')
 
+#kitchen
+with open('../keys/key_HolidaysAPI.txt') as file:
+    api_key = file.read()
+
 @app.get('/kitchen')
 def kitchen_get():
-    return render_template('kitchen.html')
+    with urllib.request.urlopen(f'{api_key}') as response:
+        data = response.read()
+    data = json.loads(data)
+    return render_template('kitchen.html',image_link=data["hdurl"], explanation=data["explanation"])
+
+#livingRoom
+with open('../keys/key_MoviesAPI.txt') as file:
+    api_key0 = file.read()
+
+with open('../keys/key_TheCatAPI.txt') as file:
+    api_key1 = file.read()
 
 @app.get('/livingRoom')
 def livingRoom_get():
-    return render_template('livingRoom.html')
+    with urllib.request.urlopen(f'{api_key}') as response:
+       data = response.read()
+    data = json.loads(data)
 
+    with urllib.request.urlopen(f'{api_key}') as response:
+       data = response.read()
+    data = json.loads(data)
+    return render_template('livingRoom.html',image_link=data["hdurl"], explanation=data["explanation"])
+
+#settings
 @app.get('/settings')
 def settings_get():
     return render_template('settings.html')
-	
+
+#register
 @app.get('/register')
 def sign_up():
     return render_template('/auth/register.html')
-    
+
 if __name__ == '__main__':
     app.run(debug=True)
