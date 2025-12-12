525cd4eeea9e67592581a6d9a0ae2d7114e9359a
Brian <brian38444@gmail.com>
2025-12-12 11:01:18 -0500

    clues table

--- app/build_db.py
+++ app/build_db.py
@@ -13,9 +13,23 @@ c = db.cursor()
 c.executescript("""
 DROP TABLE IF EXISTS profiles;
 CREATE TABLE profiles (
-    username TEXT PRIMARY KEY, 
+    username TEXT PRIMARY KEY,
     password TEXT
-);""")
+);
+
+
+DROP TABLE IF EXISTS clues;
+CREATE TABLE clues (
+    clue_id INTEGER PRIMARY KEY,
+    clue_description TEXT,
+    clue_name TEXT,
+    clue_found INTEGER
+);
+INSERT INTO clues VALUES (1, "insert clue description here", "insert name of clue if needed", 0);
+INSERT INTO clues VALUES (2, "insert clue description here", "insert name of clue if needed", 0);
+INSERT INTO clues VALUES (3, "insert clue description here", "insert name of clue if needed", 0);
+INSERT INTO clues VALUES (4, "insert clue description here", "insert name of clue if needed", 0);
+""")
 
 db.commit()
 db.close()
